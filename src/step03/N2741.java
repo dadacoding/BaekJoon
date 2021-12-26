1da52341c7373bd9c6b14cf6248ce36cd9ab9b36
dadacoding <kwond82@gmail.com>
2021-12-26 14:01:30 +0900

    03.for statement / N2741

---
+++ src/step03/N2741.java
@@ -0,0 +1,34 @@
+package step03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class N2741 {
+	public static void main(String[] args) throws IOException {
+		//N찍기 
+		
+		// 방법1. 스캐너 사용
+		
+//		Scanner scan = new Scanner(System.in);
+//		int num = scan.nextInt();
+//		scan.close();
+//		
+//		for(int i = 1 ; i <= num; i++) {
+//			System.out.println(i);
+//		}
+		
+		// 방법2. BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		br.close();
+		
+		for(int i = 1 ; i <= num; i++) {
+			System.out.println(i);
+		}
+		
+	
+	}
+}
