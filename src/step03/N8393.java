8afcc3cd2ddbca79cd5a8af6abc55910d4f02563
dadacoding <kwond82@gmail.com>
2022-01-07 23:36:05 +0900

    03. for statement / N8393

--- src/step03/N8393.java
+++ src/step03/N8393.java
@@ -2,10 +2,11 @@ package step03;
 
 import java.util.Scanner;
 
+// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램 작성
+
 public class N8393 {
 	public static void main(String[] args) {
 		
-		//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 		scan.close();
