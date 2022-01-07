f36418b30f5a981126b145aa34a6358229db274c
dadacoding <kwond82@gmail.com>
2022-01-07 23:40:03 +0900

    03. for statement / N2742

--- src/step03/N2742.java
+++ src/step03/N2742.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력
+
 public class N2742 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
